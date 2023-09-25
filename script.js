07de6398ef0e4dbe1f42075e734f7813ec324c77
Angel <amora9148@gmail.com>
2023-09-25 11:49:53 -0700

    add input log + functions

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log('hello world!');
+const choices = ['rock', 'paper', 'scissors'];
+
+function game(){
+   playRound();
+};
+
+function playRound(){
+    const playerSelection = playerChoice();
+    const computerSelection = computerChoice();
+};
+
+function playerChoice(){
+    let input = prompt('Type Rock, Paper, or Scissors');
+    input = input.toLowerCase();
+    console.log(input);
+};
+
+function computerChoice(){
+    return choices[Math.floor(Math.random()*choices.length)]
+};
+
+game();
+
+
+
